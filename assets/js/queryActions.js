915c819ac9bd4d7236ac0fc6713e9c8597f2bcde
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-10 14:21:34 -0600

    use join statements for displaying roles and employees

--- assets/js/queryActions.js
+++ assets/js/queryActions.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection(
       // MySQL username,
       user: 'root',
       // MySQL password
-      password: '',
+      password: 'aL24D7fzQi$',
       database: 'employee_db'
     },
     console.log(`Connected to the employee_db database.`)
@@ -38,7 +38,9 @@ function displayDepartments() {
 };
 
 function displayRoles() {
-    db.query(`SELECT * FROM role`, (err, result) => {
+    db.query(`SELECT role.id AS role_id, role.title, department.name AS department_name, role.salary
+    FROM role
+    JOIN department ON role.department_id = department.id;`, (err, result) => {
         if (err) {
           console.log(err);
         }
@@ -61,7 +63,13 @@ function displayRoles() {
 };
 
 function displayEmployees() {
-    db.query(`SELECT * FROM employee`, (err, result) => {
+    db.query(`SELECT emp.id AS employee_id, emp.first_name, emp.last_name, 
+    role.title, dept.name, role.salary, 
+    CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name
+    FROM employee AS emp
+    JOIN role ON emp.role_id = role.id
+    JOIN department AS dept ON role.department_id = dept.id
+    LEFT JOIN employee AS manager ON emp.manager_id = manager.id;`, (err, result) => {
         if (err) {
           console.log(err);
         }

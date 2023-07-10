e1d5751f183bc264383143f68f36896532dc8adf
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-10 13:27:42 -0600

    a table is now formatted with the data in the cli using cli-table3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const questions = require('./assets/js/questions')
+const questions = require('./assets/js/questions');
+const Table = require('cli-table3');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -9,7 +10,7 @@ const db = mysql.createConnection(
       // MySQL username,
       user: 'root',
       // MySQL password
-      password: '',
+      password: 'aL24D7fzQi$',
       database: 'employee_db'
     },
     console.log(`Connected to the employee_db database.`)
@@ -32,25 +33,67 @@ function userInput() {
 
 function displayData(action) {
     if (action.action === 'View all departments') {
-        db.query(`SELECT * FROM department`, (err, result) => {
+        db.query(`SELECT id, name FROM department`, (err, result) => {
             if (err) {
               console.log(err);
             }
-            console.log(result);
+            // Create table using cli-table3
+            let table = new Table();
+
+            // Add table headers
+            let headers = Object.keys(result[0]);
+            table.push(headers);
+        
+            // Add table rows
+            result.forEach((row) => {
+              let values = Object.values(row);
+              table.push(values);
+            });
+        
+            // Display the table
+            console.log(table.toString());
           });
     } else if (action.action === 'View all roles') {
         db.query(`SELECT * FROM role`, (err, result) => {
             if (err) {
               console.log(err);
             }
-            console.log(result);
+            // Create table using cli-table3
+            let table = new Table();
+
+            // Add table headers
+            let headers = Object.keys(result[0]);
+            table.push(headers);
+        
+            // Add table rows
+            result.forEach((row) => {
+              let values = Object.values(row);
+              table.push(values);
+            });
+        
+            // Display the table
+            console.log(table.toString());
           });
     } else if (action.action === 'View all employees') {
         db.query(`SELECT * FROM employee`, (err, result) => {
             if (err) {
               console.log(err);
             }
-            console.log(result);
+            // Create table using cli-table3
+            let table = new Table();
+
+            // Add table headers
+            let headers = Object.keys(result[0]);
+            table.push(headers);
+        
+            // Add table rows
+            result.forEach((row) => {
+              let values = Object.values(row);
+              table.push(values);
+            });
+        
+            // Display the table
+            console.log(table.toString());
           });
     }
 }
